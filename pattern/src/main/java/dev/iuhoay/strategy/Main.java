350406d8ac60c5c6e39909b9c3afe3cbfb3435a9
iuhoay <iuhoay@gmail.com>
2023-06-02 15:14:58 +0800

    Make it simple

--- pattern/src/main/java/dev/iuhoay/strategy/Main.java
+++ pattern/src/main/java/dev/iuhoay/strategy/Main.java
@@ -4,30 +4,23 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("--------------------------");
-        System.out.println("Strategy:");
-        Context context = new Context(new OperationAdd());
-        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
-        context = new Context(new OperationSubtract());
-        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
-        context = new Context(new OperationMultiply());
-        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
-
-        System.out.println("--------------------");
-
         System.out.println("Strategy Factory:");
 
-        String[] ops = {"+", "-", "*"};
+        Input[] inputs = {
+                new Input(10, 5, "+"),
+                new Input(10, 5, "-"),
+                new Input(10, 5, "*"),
+        };
 
-        Arrays.stream(ops)
-                .map(Main::execute)
-                .forEach(System.out::println);
+        Arrays.stream(inputs).forEach(Input::execute);
 
         System.out.println("Done.");
     }
 
-    private static String execute(String op) {
-        return "10 %s 5 = %d".formatted(op, StrategyFactory.getStrategy(op).execute(10, 5));
+    record Input(int a, int b, String op) {
+        public void execute() {
+            System.out.println("10 %s 5 = %d".formatted(op, StrategyFactory.getStrategy(op).execute(a, b)));
+        }
     }
 
 }
